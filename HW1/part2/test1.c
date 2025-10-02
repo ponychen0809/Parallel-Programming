71bcd6ceb4ccc51721e6896da6a3fd37266ce1bf
pony <ponychen.cs13@nycu.edu.tw>
2025-10-02 22:44:19 +0800

    update

--- HW1/part2/test1.c
+++ HW1/part2/test1.c
@@ -1,14 +1,14 @@
-// #include "test.h"
+#include "test.h"
 
-// void test1(float *a, float *b, float *c, int N) {
-//   __builtin_assume(N == 1024);
+void test1(float *a, float *b, float *c, int N) {
+  __builtin_assume(N == 1024);
 
-//   for (int i=0; i<I; i++) {
-//     for (int j=0; j<N; j++) {
-//       c[j] = a[j] + b[j];
-//     }
-//   }
-// }
+  for (int i=0; i<I; i++) {
+    for (int j=0; j<N; j++) {
+      c[j] = a[j] + b[j];
+    }
+  }
+}
 
 
 // #include "test.h"
